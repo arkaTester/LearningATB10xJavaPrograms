26798f9a8ddc0cd856d1d6769e1a8dcf07dc0fbb
arkaTester <arkapravamazumder360@gmail.com>
2025-01-20 10:57:22 +0530

    New files have been added

---
+++ src/ex_02_Java_Basics/Lab_016_Variables.java
@@ -0,0 +1,20 @@
+package ex_02_Java_Basics;
+
+public class Lab_016_Variables {
+
+    public static void main(String[] args) {
+//        int _ = 65;
+        int $ = 65;
+        int yest = 65;
+        // int -> primitive - data type - numeric
+        // it stores the whole number - without any decimal point
+        // age -> variable name or identifier
+        int this_is_a_very_long_name_in_the_class_hello_$_akakak = 89;
+
+        // =  operator Assignment ->
+        // 65 ->  literla (int) value
+
+
+
+    }
+}
