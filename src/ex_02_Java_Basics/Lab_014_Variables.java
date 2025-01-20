2a43e30b898efb8d0ea41045d73b44a322b7d819
arkaTester <arkapravamazumder360@gmail.com>
2025-01-20 10:54:16 +0530

    New files have been added

---
+++ src/ex_02_Java_Basics/Lab_014_Variables.java
@@ -0,0 +1,15 @@
+package ex_02_Java_Basics;
+
+public class Lab_014_Variables {
+    public static void main(String[] args) {
+        int a = 10;
+        // data type variable_name =  variable_value
+
+        int age = 65;
+        age = 65 + 1;
+        System.out.println(age);
+
+
+    }
+
+}
