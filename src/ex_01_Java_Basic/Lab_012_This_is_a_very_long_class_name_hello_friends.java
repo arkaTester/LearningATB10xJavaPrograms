a6fde02c7257a90dab7ab82b1022a6c0f46ebe7e
arkaTester <arkapravamazumder360@gmail.com>
2025-01-20 10:51:50 +0530

    New files have been added

---
+++ src/ex_01_Java_Basic/Lab_012_This_is_a_very_long_class_name_hello_friends.java
@@ -0,0 +1,4 @@
+package ex_01_Java_Basic;
+
+public class Lab_012_This_is_a_very_long_class_name_hello_friends {
+}
---
+++ src/ex_02_Java_Basics/Lab_013_Variables.java
@@ -0,0 +1,16 @@
+package ex_02_Java_Basics;
+
+public class Lab_013_Variables {
+
+    public static void main(String[] args) {
+        int a = 10;
+        // data type variable_name =  variable_value
+        // GST 18%
+
+        int age = 65;
+        age = 89;
+        System.out.println(age);
+
+
+    }
+}
